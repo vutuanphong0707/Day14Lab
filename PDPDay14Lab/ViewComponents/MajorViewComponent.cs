ffceaa404552a3691dd0cd8cd37e58b9bfb00a1b
Vu Tuan Phong <vutuanphong0707@gmail.com>
2025-11-15 21:28:09 +0700

    first commit

---
+++ PDPDay14Lab/ViewComponents/MajorViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PDPDay14Lab.Data;
+using PDPDay14Lab.Models;
+
+namespace PDPDay14Lab.ViewComponents
+{
+    public class MajorViewComponent : ViewComponent
+    {
+        SchoolContext db;
+        List<Major> majors;
+
+        public MajorViewComponent(SchoolContext _context)
+        {
+            db = _context;
+            majors =  db.Majors.ToList();
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("RenderMajor", majors);
+        }
+    }
+}
